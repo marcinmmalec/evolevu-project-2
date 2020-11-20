1569c4d0aa87739a2eccdbbf2d6a086c9409387a
marcinmmalec <martinmmalec@gmail.com>
2020-11-20 15:02:51 -0700

    get code up
    Will add comments later

---
+++ public/js/app.js
@@ -0,0 +1,47 @@
+console.log('Client side javascript file loaded!');
+
+/*fetch('http://localhost:3000/weather?address=boston').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error);
+        } else {
+            console.log(data.location);
+            console.log(data.forecast);
+        }
+    });
+});*/
+
+const url = 'http://localhost:3000/weather?address=';
+const url2 = 'http://weatherapp-env-1.eba-pg3pjbmp.us-west-2.elasticbeanstalk.com/weather?address=';
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+const cardTitle = document.querySelector('.card-title');
+const weatherDescription = document.querySelector('#weatherDescription');
+const weatherIcon = document.querySelector('#weatherIcon');
+const currentTemp = document.querySelector('#currentTemp');
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const location = search.value;
+    messageOne.textContent = 'Loading...';
+    //messageTwo.textContent = '';
+    fetch(`${url}${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+            } else {
+                messageOne.textContent = '';
+                //messageTwo.textContent = data.forecast;
+                cardTitle.textContent = data.location;
+                weatherDescription.textContent = data.forecast.weatherDescription;
+                weatherIcon.src = ` http://openweathermap.org/img/wn/${data.forecast.weatherIcon}@2x.png`;
+                currentTemp.textContent = data.forecast.actualTemp;
+                let d = new Date(data.forecast.utc * 1000);
+                console.log(d);
+            }
+        });
+    });
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,34 @@
+const apiKey = '';
+
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    //const url = 'http://api.weatherstack.com/current?access_key=a0994f26ee15baaa90738d4f8d08679e&query= ' + latitude + ', ' +longitude;
+    
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    
+    console.log(url);
+    
+    request({url, json: true}, (error, {body} = {}) => {
+        if (error) {
+            console.log(error);
+            callback('Unable to connect to weather services!', undefined);
+        } else if (body.error) {
+            callback(body.error.info, undefined);
+        } else {
+            let actualTemp = Math.round(body.main.temp);
+            //let feelTemp = Math.round(body.main.feels_like);
+            //let utcSeconds = body.dt;
+            //let d = new Date(utcSeconds * 1000);
+            //callback(undefined, `It is currently ${actualTemp} degrees out. It feels like ${feelTemp} degress out. It is ${body.weather[0].description}`);
+            callback(undefined, {
+                actualTemp: actualTemp,
+                weatherDescription: body.weather[0].description,
+                weatherIcon: body.weather[0].icon,
+                utc: body.dt
+            });
+        }
+    });
+}
+
+module.exports = forecast;
