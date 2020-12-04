2f4776a93e65dcee570b2f4db8549603680e97a2
Wijoyo Utomo <utomow@gmail.com>
2020-12-03 23:00:41 -0700

    Can read sensor data from database

--- Microcontroller/server/backend/server.js
+++ Microcontroller/server/backend/server.js
@@ -6,7 +6,19 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 
 const mongoose = require('mongoose')
-mongoose.connect("mongodb://localhost:27017/sensor-data",{useNewUrlParser: true, useUnifiedTopology: true})
+
+// console.log('Try to connect to database...')
+mongoose.connect("mongodb://localhost:27017/sensor-data", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true
+  // ,useFindAndModify: false
+}).catch(error => {
+  console.log("Unable to connect to database");
+  // console.log(error);
+  process.exit(1)
+})
+
 const Schema = mongoose.Schema;
 
 const sensorDataSchema = new Schema({
@@ -16,26 +28,39 @@ const sensorDataSchema = new Schema({
   pressure: Number
 })
 
-let sensorData = mongoose.model("sensorData", sensorDataSchema)
+let sensorDataModel = mongoose.model("sensordatas", sensorDataSchema)
 
 let sensorDataArray = []
 let newData = {};
 
+app.get('/', function(req, res) {
+  res.status(200).send('Sensor is not available yet');
+})
 
-app.get('/getdata/last', function(req, res) {
-  console.log('Sending last sensor data')
-  if (Object.keys(newData).length != 0) {
-    res.send(newData)
-  } else {
-    res.send('Will fetch data from database')
-    console.log("Will fetch data from database");
-  }
+
+app.get('/getdata/:sensorId', async function(req, res) {
+  const id = req.params.sensorId
+  // console.log('requested sensor id ', sensorId)
+  // const filter = {$and: [{"sensorId" : {$lte: 2}}, {"sensorId" : {$gte: 1}}]}
+  const filter = { "sensorId" : id}
+  const allsensordata = await sensorDataModel.find(filter)
+  // console.log(allsensordata);
+
+  res.send(allsensordata)
+
+  // console.log('Sending last sensor data')
+  // if (Object.keys(newData).length != 0) {
+  //   res.send(newData)
+  // } else {
+  //   res.send('Will fetch data from database')
+  //   console.log("Will fetch data from database");
+  // }
 })
 
 
 app.post('/postdata', function(req, res) {
   console.log('Receive sensor data');
-  newData = new sensorData(req.body)
+  newData = new sensorDataModel(req.body)
   console.log(newData)
   newData
       .save()
