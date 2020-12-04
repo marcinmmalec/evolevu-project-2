2f4bd46f1435325d7d5c1287a806c7c4cbac6d38
koeswanto <71404339+koeswanto@users.noreply.github.com>
2020-12-04 15:26:11 -0700

    Add files via upload

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -17,6 +17,9 @@ const forecast = (latitude, longitude, callback) => {
             callback(body.error.info, undefined);
         } else {
             let actualTemp = Math.round(body.main.temp);
+            let feelTemp = Math.round(body.main.feels_like);
+            let minTemp = Math.round(body.main.temp_min);
+            let maxTemp = Math.round(body.main.temp_max);
             //let feelTemp = Math.round(body.main.feels_like);
             //let utcSeconds = body.dt;
             //let d = new Date(utcSeconds * 1000);
@@ -25,7 +28,15 @@ const forecast = (latitude, longitude, callback) => {
                 actualTemp: actualTemp,
                 weatherDescription: body.weather[0].description,
                 weatherIcon: body.weather[0].icon,
+                humidity: body.main.humidity,
+                pressure: body.main.pressure,
+                windSpeed: body.wind.speed,
+                feelTemp: feelTemp,
+                minTemp: minTemp,
+                maxTemp: maxTemp,
+                cloud: body.clouds.all,                
                 utc: body.dt
+
             });
         }
     });
