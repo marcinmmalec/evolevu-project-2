782ce4a0058554f474608be235530b1dac03b20d
Wijoyo Utomo <utomow@gmail.com>
2020-12-08 10:54:32 -0700

    Delete SSID.h

--- Microcontroller/SSID.h
+++
@@ -1,8 +0,0 @@
-#define SSID1       "Inception"
-#define PASSWORD1   "5XE4w%ug5!PvHwyb"
-
-#define SSID2       "2 Monkeys and Dragon"
-#define PASSWORD2   "Sapim3nd3m"
-
-#define SSID3       "TELUS2340"
-#define PASSWORD3   "f99f664c77"
